0ab0cc181d5dbf2cbd12d172fbcfe0a223f03486
malevka <maleva@excbt.ru>
2019-09-02 16:33:40 +0300

    Add greeting

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
-const welcomeMsg = "Welcome to the Brain Games!";
+import {getUserName} from '../index.js';
+
+const welcomeMsg = "Welcome to the Brain Games!\n";
 console.log(welcomeMsg);
+
+const userName = getUserName();
+const greeting = `Hello, ${userName}!`;
+console.log(greeting);
+
+

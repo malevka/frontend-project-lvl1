126abd8002dd9ba83b13b860655e2aa027ee2cf6
malevka <formaleva@gmail.com>
2019-09-08 22:37:45 +0300

    change *.js after checking with eslint

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,11 +1,9 @@
 #!/usr/bin/env node
-import {getUserName} from '../index.js';
+import getUserName from '../index';
 
-const welcomeMsg = "Welcome to the Brain Games!\n";
+const welcomeMsg = 'Welcome to the Brain Games!\n';
 console.log(welcomeMsg);
 
 const userName = getUserName();
 const greeting = `Hello, ${userName}!`;
 console.log(greeting);
-
-
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export const getUserName = () => {
-    const userName = readlineSync.question('May I have your name? ');
-    return userName;
-}
+export default () => {
+  const userName = readlineSync.question('May I have your name? ');
+  return userName;
+};

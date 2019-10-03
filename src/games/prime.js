c26754c5007824b0a8fa1eca7f513c19d2d1f913
malevka <formaleva@gmail.com>
2019-10-03 11:20:05 +0300

    fix isPrime() function

--- src/games/prime.js
+++ src/games/prime.js
@@ -3,6 +3,9 @@ import { getRandomNumber, runGameProcess } from '..';
 
 const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".\n';
 const isPrime = (number) => {
+  if (number <= 1) {
+    return false;
+  }
   for (let i = 2; i < number; i += 1) {
     if (number % i === 0) {
       return false;
